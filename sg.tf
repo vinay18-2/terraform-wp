c6f42418ead6775d3f411e9046e36ee9c0e53585
Vinay Gollena <96512285+vinay18-2@users.noreply.github.com>
2024-10-21 17:06:05 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "vinaysg" {
+  vpc_id = aws_vpc.vinayvpc.id
+
+  # Inbound rules
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Egress rule for all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wp SG"
+  }
+}
