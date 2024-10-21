41f9f610a8b61c7ad7648959a71ae922888121d1
Vinay Gollena <96512285+vinay18-2@users.noreply.github.com>
2024-10-21 21:40:54 +0530

    Update wp-data.sh

--- wp-data.sh
+++ wp-data.sh
@@ -8,6 +8,7 @@ sudo usermod -aG docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo mv wordpress-docker/* /home/ec2-user/
+sudo git clone https://github.com/vinay18-2/wordpress-1.git
+sudo mv wordpress-1/* /home/ec2-user/
 cd /home/ec2-user/
 docker-compose up -d
