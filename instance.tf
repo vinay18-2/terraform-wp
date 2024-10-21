5928f1980e9f613efd319a65a32a0deda3b43112
Vinay Gollena <96512285+vinay18-2@users.noreply.github.com>
2024-10-21 16:43:42 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "vinay-1" {
+  ami           = "ami-0e54eba7c51c234f6" # Replace with your AMI ID          
+  instance_type = "t2.micro"
+  count         = 1
+  key_name      = "vinaypem"
+  vpc_security_group_ids = ["${aws_security_group.vinaysg.id}"]
+  subnet_id     = "${aws_subnet.vinaypsubnet1.id}"
+  associate_public_ip_address = true
+  user_data     = file("data.sh")
+
+  tags = {
+    Name = "tf wordpress"
+  }
+}
