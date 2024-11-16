63ad45e46202cc2e85c3e3b575b41c5c6183a1a7
Vinay Gollena <96512285+vinay18-2@users.noreply.github.com>
2024-11-16 17:43:50 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "vinay-1" {
   ami           = "ami-0e54eba7c51c234f6" # Replace with your AMI ID          
   instance_type = "t2.micro"
-  key_name      = "vinaypem"
+  key_name      = "3app"
   vpc_security_group_ids = [aws_security_group.vinaysg.id]
   subnet_id     = aws_subnet.vinaypsubnet1.id
   associate_public_ip_address = true
