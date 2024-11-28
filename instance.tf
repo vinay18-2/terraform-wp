04554142c8d190977889b628bf5662c8ef06957f
Vinay Gollena <96512285+vinay18-2@users.noreply.github.com>
2024-11-28 15:55:31 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "ram-1" {
   ami           = "ami-0166fe664262f664c" # Replace with your AMI ID          
   instance_type = "t2.micro"
-  key_name      = "3app"
+  key_name      = ""
   vpc_security_group_ids = [aws_security_group.vinaysg.id]
   subnet_id     = aws_subnet.vinaypsubnet1.id
   associate_public_ip_address = true
   user_data     = file("wp-data.sh")
 
   tags = {
-    Name = "tf ram"
+    Name = "tf vinay"
   }
 }
